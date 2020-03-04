459740c9f165cbed2b15e4b97332eaac9baa72ec
Marvin Reyes <reyemarv@gmail.com>
2020-03-04 03:23:27 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -17,7 +17,7 @@ class Doctor
     Appointment.all.select{|app| app.doctor == self}
   end 
   
-  def new_appointment(date, patient)
+  def new_appointment(patient, date)
     Appointment.new(date, patient, self)
   end 
   
